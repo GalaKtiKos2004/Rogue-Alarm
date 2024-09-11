95fb7f7eaac3df2483d53c52f87c271df30cb1f5
GalaKtiKos2004 <94827294+GalaKtiKos2004@users.noreply.github.com>
2024-09-12 05:26:53 +1000

    1st commit
    Сделана система сигнализации

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMover : MonoBehaviour
+{
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis(Horizontal), 0f, Input.GetAxis(Vertical));
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _volumeScalingSpeed = 1f;
+
+    private float _targetValue = 0f;
+
+    private void Update()
+    {
+        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, _targetValue, _volumeScalingSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMover>() != null)
+            _targetValue = 1f;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerMover>() != null)
+            _targetValue = 0f;
+    }
+}
